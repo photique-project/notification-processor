765aba47a8f6c5383260d4670f8c8c921025ecbc
BenchPress200 <jms0538@naver.com>
2026-02-11 23:40:44 +0900

    init

---
+++ src/main/java/com/benchpress200/notificationprocessor/NotificationProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.benchpress200.notificationprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationProcessorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/benchpress200/notificationprocessor/NotificationProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benchpress200.notificationprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
